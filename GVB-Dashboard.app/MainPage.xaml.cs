a9386b7bf897860a3632232b4a45c7931b94e1bc
JDAJohnsen <s1219142@student.windesheim.nl>
2025-11-27 15:51:07 +0100

    Metrolijnen met filter
    Ik heb mijn vorige commit uitgebreid met een filter optie van de metrolijnen bij de bijbehorende kaart

--- GVB-Dashboard.app/MainPage.xaml.cs
+++ GVB-Dashboard.app/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Maui.Controls;
+using System;
 
 namespace GVB_Dashboard.app;
 
@@ -7,17 +8,22 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+
+        // Standaard meteen de eerste afbeelding tonen (als er items in de picker staan)
+        if (Kaart.Items.Count > 0)
+        {
+            Kaart.SelectedIndex = 0;
+            UpdateLeftImage();
+        }
     }
 
     // === MENU HANDLERS ===
 
     private void OnDashboardTapped(object sender, TappedEventArgs e)
     {
-        // TODO: laad dashboard content
         DisplayAlert("Navigation", "Dashboard clicked", "OK");
     }
 
-    // REPORTS DROPDOWN TOGGLE
     private void OnReportsToggled(object sender, TappedEventArgs e)
     {
         if (ReportsDropdown == null || ReportsArrow == null)
@@ -29,23 +35,19 @@ public partial class MainPage : ContentPage
 
     private void OnSalesReportTapped(object sender, TappedEventArgs e)
     {
-        // TODO: vervang door echte logica
         DisplayAlert("Navigation", "Sales Report clicked", "OK");
     }
 
     private void OnAnalyticsTapped(object sender, TappedEventArgs e)
     {
-        // TODO: vervang door echte logica
         DisplayAlert("Navigation", "Analytics clicked", "OK");
     }
 
     private void OnExportsTapped(object sender, TappedEventArgs e)
     {
-        // TODO: vervang door echte logica
         DisplayAlert("Navigation", "Exports clicked", "OK");
     }
 
-    // SETTINGS DROPDOWN TOGGLE
     private void OnSettingsToggled(object sender, TappedEventArgs e)
     {
         if (SettingsDropdown == null || SettingsArrow == null)
@@ -57,20 +59,52 @@ public partial class MainPage : ContentPage
 
     private void OnProfileTapped(object sender, TappedEventArgs e)
     {
-        // TODO: vervang door echte logica
         DisplayAlert("Navigation", "Profile clicked", "OK");
     }
 
     private void OnPreferencesTapped(object sender, TappedEventArgs e)
     {
-        // TODO: vervang door echte logica
         DisplayAlert("Navigation", "Preferences clicked", "OK");
     }
 
     private void OnHelpTapped(object sender, TappedEventArgs e)
     {
-        // TODO: vervang door echte logica
         DisplayAlert("Navigation", "Help clicked", "OK");
     }
-}
 
+    // === FOTO-SELECTIE VOOR LINKER KAART ===
+
+    private void OnLeftPhotoSelected(object sender, EventArgs e)
+    {
+        UpdateLeftImage();
+    }
+
+    private void UpdateLeftImage()
+    {
+        if (Kaart.SelectedIndex < 0)
+            return;
+
+        var selectedText = Kaart.Items[Kaart.SelectedIndex];
+
+        // hier koppel je namen uit de dropdown aan bestanden in Resources/Images
+        string fileName = selectedText switch
+        {
+            "Alle Metro lijnen" => "kaart_gvb.png",
+            "Metro lijn 50" => "kaartm50_gvb.png",
+            "Metro lijn 51" => "kaartm51_gvb.png",
+            "Metro lijn 52" => "kaartm52_gvb.png",
+            "Metro lijn 53" => "kaartm53_gvb.png",
+            "Metro lijn 54" => "kaartm54_gvb.png",
+            _ => null
+        };
+
+        if (!string.IsNullOrWhiteSpace(fileName))
+        {
+            LeftSelectedImage.Source = ImageSource.FromFile(fileName);
+        }
+        else
+        {
+            LeftSelectedImage.Source = null;
+        }
+    }
+}
